78a1c6f313ab2e1457e9a5e7a9d6e013b86f9e19
pretend-m <57705304+pretend-m@users.noreply.github.com>
2023-10-19 22:23:05 -0500

    Add files via upload

---
+++ reset_navicat.sh
@@ -0,0 +1,10 @@
+rm -rf ~/Library/Preferences/com.navicat.NavicatPremium.plist
+
+regex="\.([0-9A-Z]{32})"
+[[ $(ls -a ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/ | grep '^\.') =~ $regex ]]
+
+hash=${BASH_REMATCH[1]}
+
+if [ ! -z $hash ]; then
+    rm ~/Library/Application\ Support/PremiumSoft\ CyberTech/Navicat\ CC/Navicat\ Premium/.$hash
+fi
